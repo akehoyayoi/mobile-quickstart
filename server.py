28a7fa164a4b89c68b0b30fd3ad3f83b2b97ad9d
Yohei Okaya <okaya.dcinc@gmail.com>
2017-09-22 12:41:37 +0900

    Revert "simpify"
    This reverts commit 28d33c9445e08db040e9b2c599a479f60fc1b5f4.

--- server.py
+++ server.py
@@ -44,9 +44,22 @@ def call():
   """           routed to client named CLIENT                  """
   resp = twilio.twiml.Response()
   content = request.json
+  from_value = content['from']
   to = content['to']
+  if not (from_value and to):
+    resp.say("Invalid request")
+    return str(resp)
+  from_client = from_value.startswith('client')
   caller_id = os.environ.get("CALLER_ID", CALLER_ID)
-  resp.dial(to, callerId=caller_id)
+  if not from_client:
+    # PSTN -> client
+    resp.dial(callerId=from_value).client(CLIENT)
+  elif to.startswith("client:"):
+    # client -> client
+    resp.dial(callerId=from_value).client(to[7:])
+  else:
+    # client -> PSTN
+    resp.dial(to, callerId=caller_id)
   return str(resp)
 
 @app.route('/', methods=['GET', 'POST'])
